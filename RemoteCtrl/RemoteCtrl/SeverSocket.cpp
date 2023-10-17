1f653d4390595ecaf347648b864316d8435bea6e
Haiyang-coder <shk4176@163.com>
2023-10-17 22:51:05 +0800

    1.隐藏控制命令界面，后台进行 2.完成包的设计和实现

--- RemoteCtrl/RemoteCtrl/SeverSocket.cpp
+++ RemoteCtrl/RemoteCtrl/SeverSocket.cpp
@@ -1,7 +1,99 @@
 #include "pch.h"
 #include "SeverSocket.h"
 
-//私有静态成员必须初始化
+
+
+
+CPacket::CPacket()
+{
+}
+
+CPacket::CPacket(const BYTE* pData, size_t& nSize)
+{
+	size_t i = 0;
+	for (i = 0; i < nSize; i++)
+	{
+		//填充数据头
+		if (*(WORD*)(pData + i) == 0xFEFF)
+		{
+			sHead = *(WORD*)(pData + i);
+			i += 2;
+			break;
+		}
+		//如果数据包不全，或者数据包没有完全接收到
+		if (i + 4 + 2 + 2 > nSize)
+		{
+			nSize = 0;
+			return;
+		}
+
+		nLength = *(DWORD*)(pData + i);
+		i += 4;
+		if (nLength + i > nSize)
+		{
+			//数据没有接受完整
+			nSize = 0;
+			return;
+		}
+
+		sCmd = *(WORD*)(pData + i);
+		i += 2;
+		if (nLength > 4)
+		{
+			strData.resize(nLength - 2 - 2);
+			memcpy((void*)strData.data(), pData + i, nLength - 4);
+			i += nLength - 4;
+		}
+		sSum = *(WORD*)(pData + i);
+		i += 2;
+		size_t sum = 0;
+		for (size_t j = 0; j < strData.size(); j++)
+		{
+			sum += BYTE(strData[i]) & 0xFF;
+		}
+		if (sum == sSum)
+		{
+			nSize = i;//head length data
+		}
+		nSize = 0;
+		return;
+
+		
+
+
+	}
+}
+
+CPacket::CPacket(const CPacket& packet)
+{
+	sHead = packet.sHead;//数据头
+	nLength = packet.nLength;//数据长度（从控制命令到校验的长度）
+	sCmd = packet.sCmd;//控制命令
+	strData = packet.strData;//数据
+	sSum = packet.sSum;//校验
+}
+
+CPacket& CPacket::operator=(const CPacket& packet)
+{
+	if (this == &packet)
+	{
+		return *this;
+	}
+	sHead = packet.sHead;//数据头
+	nLength = packet.nLength;//数据长度（从控制命令到校验的长度）
+	sCmd = packet.sCmd;//控制命令
+	strData = packet.strData;//数据
+	sSum = packet.sSum;//校验
+	return *this;
+}
+
+CPacket::~CPacket()
+{
+}
+
+
+
+//私有静态成员必须初始化 
 CSeverSocket* CSeverSocket::m_pinstance = nullptr;
 CSeverSocket::CHelper CSeverSocket::m_helper;
 CSeverSocket::CSeverSocket()
@@ -96,25 +188,38 @@ void CSeverSocket::releaseInstance()
 
 	}
 }
-
+#define BUFFER_SIZE 4096
 int CSeverSocket::DealCommand()
 {
 	if (m_client == -1)
 	{
 		return -1;
 	}
-	char buffer[1024];
+	//char buffer[1024];
+	char* buffer = new char[BUFFER_SIZE];
+	memset(buffer, 0, BUFFER_SIZE);
+	size_t index = 0;
 	while (true)
 	{
-		int len  = recv(m_client, buffer, sizeof(buffer), 0);
+		size_t len  = recv(m_client, buffer + index, BUFFER_SIZE - index, 0);
 		if (len <= 0)
 		{
 			return -1;
 		}
-		
+		index += len;
+		len = index;
+		CPacket packet((BYTE*)buffer, len);
+		m_packet = packet;
+		if (len > 0)
+		{
+			memmove(buffer, buffer + len, BUFFER_SIZE - len);
+			index -= len;
+			return packet.sCmd;
+		}
 		
 	}
-	closesocket(m_client);
+	
+	return -1;
 }
 
 bool CSeverSocket::Send(const char* pData, size_t nize)
--- RemoteCtrl/RemoteCtrl/SeverSocket.h
+++ RemoteCtrl/RemoteCtrl/SeverSocket.h
@@ -44,8 +44,30 @@ private:
 	 static CHelper m_helper;
 	 SOCKET m_serv_sock;
 	 SOCKET m_client;
+	 CPacket m_packet;
 };
 
-extern CSeverSocket server;
+
+
+class CPacket
+{
+public:
+	CPacket();
+	CPacket(const BYTE* , size_t& nSize);
+	CPacket(const CPacket& packet);
+	CPacket& operator=(const CPacket& packet);
+	~CPacket();
+public:
+	WORD sHead = 0;//数据头
+	DWORD nLength = 0;//数据长度（从控制命令到校验的长度）
+	WORD sCmd = 0;//控制命令
+	std::string strData = "";//数据
+	WORD sSum = 0;//校验
+
+private:
+
+};
+
+
 
  
