be1f7276c274bdc8a4a8b3b9633e35c7901cbd2e
孙恒康 <shk4176@163.com>
2023-11-22 19:38:29 +0800

    实现通过为文件复制到startup文件夹来现实开机启动。

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -15,21 +15,11 @@
 // 唯一的应用程序对象 这是fiest的代码
 
 CWinApp theApp;
-
-void ChooseAutoInvoke()
+void WriteRegisterTable(const CString& strPath)
 {
-    CString strpath = CString(_T("C:\\Windows\\SysWOW64\\RemoteCtrl.exe"));
-    if (PathFileExists(strpath))
-    {
-        return;
-    }
+    int ret = 0;
     CString strBugkey = _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
-    CString strInfo = _T("该程序只能用于合法用途");
-    int ret = MessageBox(NULL, strInfo, _T("警告"), MB_YESNOCANCEL | MB_ICONWARNING | MB_TOPMOST);
-    if (ret != IDOK)
-    {
-        exit(0);
-    }
+    CString strpath = CString(_T("C:\\Windows\\SysWOW64\\RemoteCtrl.exe"));
     char sPath[MAX_PATH] = "";
     char sSys[MAX_PATH] = "";
     std::string strExe = "\\RemoteCtrl.exe ";
@@ -46,9 +36,9 @@ void ChooseAutoInvoke()
         MessageBox(NULL, _T("设置自动开机启动失败，是否权限不足 \r\n 程序启动失败!"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
         exit(0);
     }
- 
-   
-    ret = RegSetValueEx(hkey, _T("RemoteCtrl"), 0, REG_SZ, (BYTE*)(LPCTSTR)strpath, strpath.GetLength()* sizeof(TCHAR));
+
+
+    ret = RegSetValueEx(hkey, _T("RemoteCtrl"), 0, REG_SZ, (BYTE*)(LPCTSTR)strpath, strpath.GetLength() * sizeof(TCHAR));
     if (ret != ERROR_SUCCESS)
     {
         RegCloseKey(hkey);
@@ -56,7 +46,38 @@ void ChooseAutoInvoke()
         exit(0);
     }
     RegCloseKey(hkey);
-
+}
+void WriteStartUpDir(const CString& strPath)
+{
+    
+    CString strcmd =  GetCommandLine();
+    strcmd.Replace(_T("\""), _T(""));
+    BOOL ret = CopyFile(strcmd, strPath, FALSE);
+    if (ret == FALSE)
+    {
+        MessageBox(NULL, _T("复制文件失败，是否权限不足\r\n"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+        exit(0);
+    }
+}
+void ChooseAutoInvoke()
+{
+    //开机启动的权限和启动用户的权限是一样的，权限不够不可以
+    //开机启动对环境变量有影响，如果依赖动态库，可能启动失败
+    CString strPathStartup = _T("C:\\Users\\lenovo\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\");
+    CString strpath = CString(_T("C:\\Windows\\SysWOW64\\RemoteCtrl.exe"));
+    if (PathFileExists(strpath))
+    {
+        return;
+    }
+  
+    CString strInfo = _T("该程序只能用于合法用途");
+    int ret = MessageBox(NULL, strInfo, _T("警告"), MB_YESNOCANCEL | MB_ICONWARNING | MB_TOPMOST);
+    if (ret != IDOK)
+    {
+        exit(0);
+    }
+    //WriteRegisterTable();
+    WriteStartUpDir(strPathStartup);
    
 }
 
@@ -83,7 +104,7 @@ int main()
         {
             // TODO: 在此处为应用程序的行为编写代码。sadasdjhi
             CCommand command;
-            ChooseAutoInvoke();
+            //ChooseAutoInvoke();
             CSeverSocket* pserver = CSeverSocket::getInstance();
             int ret = pserver->RunFunc(&CCommand::RunCommand, &command);
             switch (ret)
